dc01c1f2264c4b9158aa93d63d5817bd29e23a85
Sachinn-9700 <sachinn.bashetti@gmail.com>
2026-05-25 01:17:11 +0530

    integrate PostgreSQL with auth service

---
+++ backend/auth-service/app/core/database.py
@@ -0,0 +1,12 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+DATABASE_URL = "postgresql://cloudcart_user:cloudcart_password@localhost:5432/cloudcart_auth"
+
+engine = create_engine(DATABASE_URL)
+
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine
+)
--- backend/auth-service/app/main.py
+++ backend/auth-service/app/main.py
@@ -1,4 +1,7 @@
 from fastapi import FastAPI
+from sqlalchemy import text
+
+from app.core.database import engine
 
 app = FastAPI(
     title="CloudCart Auth Service",
@@ -11,3 +14,21 @@ def health_check():
         "status": "healthy",
         "service": "auth-service"
     }
+
+@app.get("/db-health")
+def db_health_check():
+    try:
+        with engine.connect() as connection:
+            connection.execute(text("SELECT 1"))
+
+        return {
+            "database": "connected"
+        }
+
+    except Exception as error:
+        return {
+            "database": "failed",
+            "error": str(error)
+        }
+
+
