fe29b998c99bec5b278e19505bae9d780066ae88
Sachinn-9700 <sachinn.bashetti@gmail.com>
2026-05-27 12:05:30 +0530

    Add baseline Docker configuration for CloudCart services

--- backend/auth-service/app/core/database.py
+++ backend/auth-service/app/core/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "postgresql://cloudcart_user:cloudcart_password@localhost:5432/cloudcart_auth"
+DATABASE_URL = "postgresql://cloudcart_user:cloudcart_password@10.0.2.15:5432/cloudcart_auth"
 
 engine = create_engine(DATABASE_URL)
 
